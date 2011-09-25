ad3df483de526ae33b1dd101e9dea0dbe291e054
Tobias Schmidt <tobidt@gmail.com>
2011-09-25 09:53:22 +0200

    add thin, use normal sinatra mode

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem "sinatra"
 gem "sinatra-activerecord"
 gem "faraday"
 gem "aasm"
+gem "thin"
 
 group :production do
   gem "pg", "0.11.0"
--- hangartist.rb
+++ hangartist.rb
@@ -1,47 +1,43 @@
-require 'sinatra/base'
+require 'sinatra'
 require 'sinatra/activerecord'
 
 require_relative "lib/echonest"
 require_relative "app/artist"
 require_relative "app/game"
 
+set :root,   File.expand_path(File.dirname(__FILE__))
+set :public, File.expand_path("public", settings.root)
+
 Echonest.configure do |config|
   config.api_root = "http://developer.echonest.com/api/v4"
   config.api_key = "WIU43FOYVQXSRNV1V"
 end
 
-class HangArtist < Sinatra::Base
-  set :root,   File.expand_path(File.dirname(__FILE__))
-  set :public, File.expand_path("public",  settings.root)
+get '/favicon.ico' do
+end
 
-  get '/favicon.ico' do
+get '/' do
+  erb :index
+end
+
+post '/' do
+  if @game = Game.create
+    redirect "/#{@game.id}"
+  else
+    "FAILURE"
   end
+end
 
-  get '/' do
-    erb :index
+get '/:id' do
+  @game = Game.find(params[:id])
+  erb :show
+end
+
+get '/:id/solve' do
+  @game = Game.find(params[:id])
+  if @game.artist.name == params[:artist]
+    "FUCK YEAH!"
+  else
+    redirect "/#{@game.id}"
   end
-
-  post '/' do
-    if @game = Game.create
-      redirect "/#{@game.id}"
-    else
-      "FAILURE"
-    end
-  end
-
-  get '/:id' do
-    @game = Game.find(params[:id])
-    erb :show
-  end
-
-  get '/:id/solve' do
-    @game = Game.find(params[:id])
-    if @game.artist.name == params[:artist]
-      "FUCK YEAH!"
-    else
-      redirect "/#{@game.id}"
-    end
-  end
-
-  run!
 end
